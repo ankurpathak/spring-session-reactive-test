513764055f3e49433da174476a826144b28d14ba
Ankur Pathak <ankurpathak@live.in>
2018-10-10 19:35:31 +0530

    normal web session

--- src/main/java/com/ankurpathak/springsessionreactivetest/SessionConfig.java
+++ src/main/java/com/ankurpathak/springsessionreactivetest/SessionConfig.java
@@ -3,21 +3,34 @@ package com.ankurpathak.springsessionreactivetest;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.session.ReactiveMapSessionRepository;
+import org.springframework.session.ReactiveSessionRepository;
+import org.springframework.session.SessionRepository;
+import org.springframework.session.config.annotation.web.server.EnableSpringWebSession;
 import org.springframework.web.server.session.HeaderWebSessionIdResolver;
 import org.springframework.web.server.session.WebSessionIdResolver;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 @Configuration
-//@EnableRedisWebSession
+@EnableSpringWebSession
 public class SessionConfig  {
 
-    /*
+
 
     @Bean
     public WebSessionIdResolver httpSessionStrategy() {
-       // HeaderWebSessionIdResolver sessionIdResolver = new HeaderWebSessionIdResolver();
-     //   sessionIdResolver.setHeaderName("X-Auth-Token");
+        HeaderWebSessionIdResolver sessionIdResolver = new HeaderWebSessionIdResolver();
+        sessionIdResolver.setHeaderName("X-Auth-Token");
         return sessionIdResolver;
-    }*/
+    }
+
+
+    @Bean
+    public ReactiveSessionRepository sessionRepository() {
+        return new ReactiveMapSessionRepository(new ConcurrentHashMap<>());
+    }
+
 
 
 
